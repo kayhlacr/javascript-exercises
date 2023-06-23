d18440c4e3a78661eb456cc3da03bf8c6d14ec98
Kayhlacr <kayhla@student.dev-island.org>
2023-06-22 19:44:21 -1000

    Passed reserveString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function () {};
+const repeatString = function (str, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let resultsStr = "";
+  for (i = 0; i < times; i++) {
+    resultsStr += str;
+  }
+
+  return resultsStr;
+};
 
 // Do not edit below this line
 module.exports = repeatString;
