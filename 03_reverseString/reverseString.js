51a6c38ba1f63c9ec3fdfeaeebe7d4fcc8e1ec19
Kayhlacr <kayhla@student.dev-island.org>
2023-06-22 20:06:02 -1000

    Passed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let resultsStr = "";
+  for (let i = 0; i < str.length; i++) {
+    resultsStr += str[str.length - i - 1];
+  }
+  return resultsStr;
 };
 
 // Do not edit below this line
