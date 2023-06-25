92af02e4aa9f6d21cbd29224597727c1a9d39c00
Kayhlacr <kayhla@student.dev-island.org>
2023-06-24 19:56:17 -1000

    Passed Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  const cConversion = (fTemp - 32) * (5 / 9);
+  let cRounded = Math.round(cConversion * 10) / 10;
+  {
+    return cRounded;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  const fConversion = cTemp * (9 / 5) + 32;
+  let fRounded = Math.round(fConversion * 10) / 10;
+  {
+    return fRounded;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
