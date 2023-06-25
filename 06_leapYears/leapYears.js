66155c3ccb99da9d12ced424913002f81e036dd1
Kayhlacr <kayhla@student.dev-island.org>
2023-06-24 19:28:41 -1000

    passed Leap Years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (yearInput) {
+  let leapChecked;
+  if ((yearInput % 4 === 0 && yearInput % 100 !== 0) || yearInput % 400 === 0) {
+    return (leapChecked = true);
+  } else {
+    return (leapChecked = false);
+  }
+  return leapChecked;
 };
 
 // Do not edit below this line
