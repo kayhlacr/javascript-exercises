7736f0fb4430795bf96f10d7d358e577be672ae1
Kayhlacr <kayhla@student.dev-island.org>
2023-06-24 17:10:12 -1000

    Passed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let error = "ERROR";
 
+  let bigValue = Math.max(num1, num2);
+  let smallValue = Math.min(num1, num2);
+
+  if (typeof num1 === "string" && typeof num2 === "string") {
+    return error;
+  } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  } else if (bigValue >= 0 && smallValue >= 0) {
+    for (let i = smallValue; i <= bigValue; i++) {
+      sum = sum + i;
+    }
+  } else {
+    return error;
+  }
+
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
