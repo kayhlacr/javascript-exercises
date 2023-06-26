5d7d306a8279aa1db5a0875a80da6cccf2cc0a4f
Kayhlacr <kayhla@student.dev-island.org>
2023-06-25 16:24:45 -1000

    Passed Remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  let newArr = [];
 
+  for (let i = 0; i < array.length; i++) {
+    if (!values.includes(array[i])) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
 const convertToCelsius = function (fTemp) {
   const cConversion = (fTemp - 32) * (5 / 9);
-  let cRounded = Math.round(cConversion * 10) / 10;
+  let cRounded = Math.round(cConversion * 10) / 10; //to round to one decimal place
   {
     return cRounded;
   }
@@ -8,7 +8,7 @@ const convertToCelsius = function (fTemp) {
 
 const convertToFahrenheit = function (cTemp) {
   const fConversion = cTemp * (9 / 5) + 32;
-  let fRounded = Math.round(fConversion * 10) / 10;
+  let fRounded = Math.round(fConversion * 10) / 10; //to round to one decimal place
   {
     return fRounded;
   }
