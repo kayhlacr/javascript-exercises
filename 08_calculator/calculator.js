b8b900015587aca279f46c88375e2c713b58b8c4
Kayhlacr <kayhla@student.dev-island.org>
2023-07-17 23:09:06 -1000

    calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (...nums) {
+  let sum = 1;
+  for (let i = 0; i < nums.length; i++) {
+    sum *= nums[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) return 1;
+  for (let i = num - 1; i > +1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
