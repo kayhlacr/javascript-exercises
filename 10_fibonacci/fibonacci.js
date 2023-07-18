e49e87db47cc0fd6ccd3b78ae3cfe185b9866bfe
Kayhlacr <kayhla@student.dev-island.org>
2023-07-18 12:09:06 -1000

    had to look at solution file

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,11 @@
-const fibonacci = function () {};
+const fibonacci = function (total) {
+  if (total < 0) return "OOPS";
+  const fibPart = [0, 1];
+  for (let i = 1; i < total; i++) {
+    fibPart.push(fibPart[i] + fibPart[i - 1]);
+  }
+  return fibPart[total];
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
