e0f05f911c3354804569e09911270ee4f0857e81
Kayhlacr <kayhla@student.dev-island.org>
2023-07-18 11:30:41 -1000

    getTheTitles passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  const getBooks = books.map((book) => book.title);
+  return getBooks;
 };
 
 // Do not edit below this line
